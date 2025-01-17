87bff37d61c04ac8ace9f059f1bf25544aa5e220
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2025-01-18 00:32:17 +0100

    first steps for this app's architecture

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,12 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomeScreen from "screens/HomeScreen";
+import HomeScreen from "screens/HomeScreen/HomeScreen";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
 	return (
-		<Stack.Group screenOptions={{ headerLargeTitle: true }}>
-			<Stack.Screen
-				name='Home'
-				component={HomeScreen}
-				options={{ title: "Home" }}
-			/>
-			<Stack.Screen
-				name='Search'
-				component={SearchScreen}
-				options={{ title: "Search Videos" }}
-			/>
-			<Stack.Screen
-				name='Detail'
-				component={DetailScreen}
-				options={{ title: "Video Details" }}
-			/>
-		</Stack.Group>
+		<Stack.Navigator>
+			<Stack.Screen name='Home' component={HomeScreen} />
+		</Stack.Navigator>
 	);
 }
