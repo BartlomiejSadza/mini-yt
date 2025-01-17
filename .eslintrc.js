d8414eb80541810a539f8a37150c67706d790eeb
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2025-01-17 22:07:56 +0100

    project setup

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+	ignorePatterns: ["/dist/*"],
+	root: true,
+	extends: [
+		"expo",
+		"plugin:react-native/all",
+		"plugin:react-hooks/recommended",
+	],
+	parserOptions: {
+		ecmaVersion: 2021,
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	env: {
+		"react-native/react-native": true,
+		es6: true,
+	},
+	plugins: ["react-native", "react-hooks", "prettier"],
+	rules: {
+		"react-native/no-inline-styles": "warn",
+		"react-native/sort-styles": "off",
+	},
+};
