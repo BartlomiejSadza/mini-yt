2a56b50c18b760a0b8010820abc3c915e899a2f6
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2025-02-18 23:33:07 +0100

    ahh ten pasek

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,6 +15,7 @@ export default function TabLayout() {
 				tabBarLabelStyle: {
 					fontFamily: Fonts.Regular,
 					fontSize: FontSizes.md,
+					flex: 1,
 					marginTop: 5,
 				},
 				tabBarActiveTintColor: Colors.white,
