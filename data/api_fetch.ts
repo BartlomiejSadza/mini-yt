a290b4507be231317a0e1492d1f87600fc62cd9b
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2025-02-18 19:52:10 +0100

    no działa

--- data/api_fetch.ts
+++ data/api_fetch.ts
@@ -1,38 +1,19 @@
-const API_KEY = "AIzaSyAXr30-BsbSn0PXxBo1EJ0xhy2xC-whyCs";
+import axios from "axios";
+import { config } from "../config/env";
+import { YouTubeResponse } from "../types/youTubeResponse";
 
-interface YouTubeResponse {
-	items: YouTubeItem[];
-}
-
-interface YouTubeItem {
-	kind: string;
-	etag: string;
-	id: {
-		kind: string;
-		videoId: string;
-	};
-	snippet: {
-		publishedAt: string;
-		channelId: string;
-		title: string;
-		description: string;
-		thumbnails: {
-			default: { url: string };
-			medium: { url: string };
-			high: { url: string };
-		};
-		channelTitle: string;
-		liveBroadcastContent: string;
-		publishTime: string;
-	};
-}
-
-async function fetchVideos(query: string) {
-	const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&maxResults=100&key=${API_KEY}`;
-
-	const response = await fetch(url);
-	const data: YouTubeResponse = await response.json();
+export async function fetchVideos(query: string) {
+	const { data } = await axios.get<YouTubeResponse>(
+		`${config.YOUTUBE_API_URL}/search`,
+		{
+			params: {
+				part: "snippet",
+				q: query,
+				type: "video",
+				maxResults: 100,
+				key: config.YOUTUBE_API_KEY,
+			},
+		}
+	);
 	return data.items;
 }
-
-export default fetchVideos;
